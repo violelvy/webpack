c990ad64f5c9e5f588e59d44ddc010ab91fa15d7
Violetlvy <39662476+violelvy@users.noreply.github.com>
2019-07-07 01:34:18 +0800

    更改文件

---
+++ index.js
@@ -0,0 +1,9 @@
+// ES Moudule 模块引入方式
+// webpack 核心定义是模块打包工具
+
+import Header from './header.js';
+import Sidebar from './sidebar.js';
+import Content from './content.js'
+new Header();
+new Sidebar();
+new Content()
