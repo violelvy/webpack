951d3560b995ee06b63693cc4507f79174f33461
Violetlvy <39662476+violelvy@users.noreply.github.com>
2019-07-07 04:31:52 +0800

    update

---
+++ index/webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports = {
+    entry: './src/index.js', //从哪个文件开始打包
+    output: {     //出口
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,"dist")   //打包出的文件放在哪个文件夹下，绝对路径
+    }
+}
